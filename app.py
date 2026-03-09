f0153b6d8b1fc432d3651984be840bb9072538ae
Sho-Wadamori <23196@burnside.school.nz>
2026-03-10 11:52:33 +1300

    added card editing and deleting, fixed URLs to be more consistent

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # import libraries
 from flask import Flask, g, redirect, render_template, request, url_for
-from werkzeug.utils import secure_filename  # for file uploads
+# from werkzeug.utils import secure_filename  # for file uploads
 from datetime import datetime, timezone  # for time formatting
 import sqlite3
 
@@ -64,9 +64,10 @@ def format_date(date_string):
     dt = datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
     dt = dt.replace(tzinfo=timezone.utc)
 
-    # convert to local time 
+    # convert to local time
     local_dt = dt.astimezone()  # system local zone
     return local_dt.strftime("%d/%b/%Y")
+
 
 # homepage
 @app.route('/')
@@ -107,19 +108,44 @@ def Deck(id):
     return render_template("deck.html", results=results, deck_info=deck_info, time_ago=time_ago, format_date=format_date)
 
 
+@app.route('/decks/<int:id>/cards/<int:card_id>/delete/', methods=['POST'])
+def deleteCard(id, card_id):
+    # delete the card with the inputted card id
+    sql = """
+            DELETE FROM Flashcards WHERE card_ID = ?;
+        """
+    get_db().execute(sql, (card_id,))
+    get_db().commit()
+    # redirect to the deck page
+    return redirect(url_for('Deck', id=id))
+
+
+@app.route('/decks/<int:id>/delete/', methods=['POST'])
+def deleteDeck(id):
+    # delete the deck with the inputted deck id
+    sql = """
+            DELETE FROM Decks WHERE deck_ID = ?;
+        """
+    get_db().execute(sql, (id,))
+    get_db().commit()
+    # redirect to the deck page
+    return redirect(url_for('Decks'))
+
+
+# study a single card based on the index
 @app.route('/decks/<int:id>/study/<int:index>/')
 def Study(id, index):
     # get all the card id, question, answer, and creation date for inputted deck id
     sql = """
-            SELECT card_ID, card_question, card_answer, card_creation
+            SELECT card_ID, card_question, card_answer, card_creation, card_hint
             FROM Flashcards
             WHERE card_deckID = ?;
         """
     results = query_db(sql, (id,))
 
-    total = len(results) # total number of cards in the deck
+    total = len(results)  # total number of cards in the deck
 
-    card = results[index] # get the current card info based on the index
+    card = results[index]  # get the current card info based on the index
 
     return render_template(
         "card.html",
@@ -168,43 +194,124 @@ def createDeck():
 # create a new card for a specific deck and add it to the database
 @app.route('/decks/<int:id>/create/', methods=['GET', 'POST'])
 def createCard(id):
+
+    # get the deck name of the inputted deck id
+    sql_deck = """
+            SELECT deck_name, deck_ID, deck_creation FROM Decks WHERE deck_ID = ?;
+        """
+    deck_info = query_db(sql_deck, (id,))[0]
+
     # if request method is POST, get the form data and insert into database
     if request.method == "POST":
         card_question = request.form['cardQuestion']
         card_answer = request.form['cardAnswer']
+        card_hint = request.form['cardHint']
 
         # check if the form data is not empty
         if not card_question or not card_answer:
             # reload the page with the error message
             error = "Both fields are required."
-            sql = """
-                    SELECT card_ID, card_question, card_answer, card_creation
-                    FROM Flashcards;
-                """
-            result = query_db(sql)
 
-            return render_template("cardCreate.html", results=result, error=error)
+            return render_template("cardCreate.html", error=error, deck_info=deck_info)
 
         else:
             sql = """
-                    INSERT INTO Flashcards (card_question, card_answer, card_deckID, card_creation)
-                    VALUES (?, ?, ?, datetime('now'));
+                    INSERT INTO Flashcards (card_question, card_answer, card_deckID, card_creation, card_hint)
+                    VALUES (?, ?, ?, datetime('now'), ?);
                 """
-            get_db().execute(sql, (card_question, card_answer, id))
+
+            get_db().execute(sql, (card_question, card_answer, id, card_hint))
             get_db().commit()
             # redirect to the deck page
             return redirect(url_for('Deck', id=id))
 
     # if request method is GET, return the sql results
     else:
+
+        # return deckinfo
+        return render_template("cardCreate.html", deck_info=deck_info)
+
+
+# edit a deck and update the database
+@app.route('/decks/<int:id>/edit/', methods=['GET', 'POST'])
+def editDeck(id):
+    # if request method is POST, get the form data and insert into database
+    if request.method == "POST":
+        deck_name = request.form['deckName']
+        deck_description = request.form['deckDescription']
+
+        # check if the form data is not empty
+        if not deck_name:
+            # reload the page with the error message
+            error = "A deck name is required."
+            return render_template("deck.html", error=error, id=id)
+
+        else:
+            sql = """
+                    UPDATE Decks
+                    SET deck_name = ?, deck_description = ?
+                    WHERE deck_ID = ?;
+                """
+            get_db().execute(sql, (deck_name, deck_description, id))
+            get_db().commit()
+            # redirect to the decks list page
+            return redirect(url_for('Decks'))
+    # if request method is GET, return the sql results
+    else:
         # get all the decks id, name, description, and creation date
         sql = """
-                SELECT card_ID, card_question, card_answer, card_creation
-                FROM Flashcards;
+                SELECT deck_ID, deck_name, deck_description, deck_creation
+                FROM Decks
+                WHERE deck_ID = ?;
             """
-        result = query_db(sql)
+        result = query_db(sql, (id,))
         # return the results
-        return render_template("cardCreate.html", results=result)
+        return render_template("deckEdit.html", results=result)
+
+
+# edit a card and update the database
+@app.route('/decks/<int:id>/cards/<int:card_id>/edit/', methods=['GET', 'POST'])
+def editCard(id, card_id):
+    # get the deck name of the inputted deck id
+    sql_deck = """
+            SELECT deck_name, deck_ID, deck_creation FROM Decks WHERE deck_ID = ?;
+        """
+    deck_info = query_db(sql_deck, (id,))[0]
+
+    # if request method is POST, get the form data and insert into database
+    if request.method == "POST":
+        card_question = request.form['cardQuestion']
+        card_answer = request.form['cardAnswer']
+        card_hint = request.form['cardHint']
+
+        # check if the form data is not empty
+        if not card_question or not card_answer:
+            # reload the page with the error message
+            error = "Both fields are required."
+
+            sql_card = "SELECT card_ID, card_question, card_answer, card_creation, card_hint FROM Flashcards WHERE card_ID = ?;"
+            card = query_db(sql_card, (card_id,), one=True)
+            return render_template("cardEdit.html", error=error, deck_info=deck_info, cards=card)
+
+        else:
+            sql = """
+                    UPDATE Flashcards
+                    SET card_question = ?, card_answer = ?, card_hint = ?
+                    WHERE card_ID = ? AND card_deckID = ?;
+                """
+
+            get_db().execute(sql, (card_question, card_answer, card_hint, card_id, id))
+            get_db().commit()
+            # redirect to the deck page
+            return redirect(url_for('Deck', id=id))
+
+    # if request method is GET, return the sql results
+    else:
+
+        # return cardinfo
+        sql_card = "SELECT card_ID, card_question, card_answer, card_creation, card_hint FROM Flashcards WHERE card_ID = ?;"
+        card = query_db(sql_card, (card_id,), one=True)
+        return render_template("cardEdit.html", deck_info=deck_info, cards=card)
 
 
 # only run the app if app.py is executed directly
