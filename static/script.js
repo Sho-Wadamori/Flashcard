30f7ca4c72a06771f09eda28fc78605ba8186f4b
Sho-Wadamori <23196@burnside.school.nz>
2026-04-13 18:13:14 +1200

    minor improvements to styling, added more comments, removed unnecessary and unused javascript

--- static/script.js
+++ static/script.js
@@ -1,27 +1,7 @@
 // load html brefore running
 document.addEventListener("DOMContentLoaded", () => {
 
-    // ---------- Render Scientific Text for each instance (ONLY STATIC) ----------
-    document.querySelectorAll(".scientific").forEach(i => {
-        if (i.textContent.includes('\\(')) { 
-            MathJax.typesetPromise([i]);
-        }
-    });
-
-
-    // ---------- Password reveal system ----------
-    function passwordReveal() {
-        var password = document.getElementById("password");
-        var confirm = document.getElementById("confirm_password");
-
-        if (password.type === "password") {
-            password.type = "text";
-            confirm.type = "text";
-        } else {
-            password.type = "password";
-            confirm.type = "password";
-        }
-    }
+    
 
 });
 
