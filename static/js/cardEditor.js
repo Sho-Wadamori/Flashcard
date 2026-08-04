38145d27f77e0278c60d6c00943375f404f35f46
Sho-Wadamori <23196@burnside.school.nz>
2026-08-04 19:37:50 +1200

    moved remaining inline css into css files

--- static/js/cardEditor.js
+++ static/js/cardEditor.js
@@ -81,12 +81,6 @@ const formFields = [
     {editable: cardAnswerEditable, input: cardAnswerInput},
 ];
 
-// const images = [
-//     {input: ImageEditable, hiddenInput: cardImageInput, original: originalImage, form: "cardForm"},
-//     {input: ImageEditable, hiddenInput: quizImageInput, original: originalImage, form: "quizForm"},
-//     {input: ImageEditable, hiddenInput: tfImageInput, original: originalImage, form: "tfForm"}
-// ];
-
 // last focused variable for inputter
 let lastFocused = null;
 
